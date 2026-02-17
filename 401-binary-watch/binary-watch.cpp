b8733c21e52e46912ab2147d5a3d37e3069f296e
Ayush Bhardwaj <158686408+AyushBhardwajj@users.noreply.github.com>
2026-02-17 11:19:53 +0530

    Time: 500 ms (1.93%) | Memory: 11.4 MB (5.28%) - LeetSync

---
+++ 401-binary-watch/binary-watch.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void solve(int hrs,int minutes,set<string> &st,int k){
+        if(k==0){
+            if(hrs<=11 && minutes<=59){
+                string h = to_string(hrs);
+                string min = to_string(minutes);
+
+                if(min.length()==1){
+                    min = "0"+min;
+                }
+
+                h = h+":"+min;
+
+                st.insert(h);
+            }
+            return;
+        }
+
+        if(k>0){
+            for(int i=0;i<4;i++){
+                if(hrs&(1<<i))continue;
+                int nhrs = hrs|(1<<i);
+                solve(nhrs,minutes,st,k-1);
+            }
+        }
+
+        if(k>0){
+            for(int i=0;i<6;i++){
+                if(minutes&(1<<i))continue;
+                int mint = minutes|(1<<i);
+                solve(hrs,mint,st,k-1);
+            }
+        }
+
+        return ;
+    }
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> ans;
+        set<string> st;
+        solve(0,0,st,turnedOn);
+        for(auto it:st){
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
