5d33acdaa35fcd9862274f263e1c7292b73ace50
Ayush Bhardwaj <158686408+AyushBhardwajj@users.noreply.github.com>
2026-02-17 11:22:22 +0530

    Time: 238 ms (1.93%) | Memory: 11.3 MB (5.28%) - LeetSync

--- 401-binary-watch/binary-watch.cpp
+++ 401-binary-watch/binary-watch.cpp
@@ -21,6 +21,7 @@ public:
             for(int i=0;i<4;i++){
                 if(hrs&(1<<i))continue;
                 int nhrs = hrs|(1<<i);
+                if(nhrs>11)break;
                 solve(nhrs,minutes,st,k-1);
             }
         }
@@ -29,6 +30,7 @@ public:
             for(int i=0;i<6;i++){
                 if(minutes&(1<<i))continue;
                 int mint = minutes|(1<<i);
+                if(mint>59)break;
                 solve(hrs,mint,st,k-1);
             }
         }
