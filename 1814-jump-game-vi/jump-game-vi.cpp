e7f6368fb5146bf3c7c17e254bdc70f01c899e2f
Ayush Bhardwaj <158686408+AyushBhardwajj@users.noreply.github.com>
2026-05-15 12:49:33 +0530

    Time: 483 ms (5.10%) | Memory: 191.8 MB (5.10%) - LeetSync

---
+++ 1814-jump-game-vi/jump-game-vi.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        multiset<int> mt;
+        map<int,int> mp;
+
+        int n = nums.size();
+
+        mp[0] = nums[0];
+        int start = 0;
+        mt.insert(nums[0]);
+
+        for(int i=1;i<n;i++){
+            if((i-start) > k){
+                auto it = mt.find(mp[start]);
+                mt.erase(it);
+                start++;
+            }
+
+            auto val  = mt.rbegin();;
+
+            int curr = nums[i]+ *val;
+
+            mp[i] = curr;
+
+            mt.insert(curr);
+        } 
+
+        return mp[n-1];
+    }
+};
