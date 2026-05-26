f192617b74d6c65df154c31af929948a0298477a
Ayush Bhardwaj <158686408+AyushBhardwajj@users.noreply.github.com>
2026-05-26 23:28:27 +0530

    Time: 283 ms (89.66%) | Memory: 73.6 MB (21.36%) - LeetSync

--- 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    void solve(int start,int end,int cnt,int sum,vector<int> &nums,vector<vector<int>> &dp){
-
-        dp[cnt].push_back(sum);
-
-        for(int i=start;i<end;i++){
-            solve(i+1,end,cnt+1,sum+nums[i],nums,dp);
+    void solve(int start,int end,int tot,int cnt,vector<int> &nums,vector<vector<int>> &dp){
+        if(start == end){
+            dp[cnt].push_back(tot);
+            return;
         }
 
-        return;
+        solve(start+1,end,tot+nums[start],cnt+1,nums,dp);
+        solve(start+1,end,tot,cnt,nums,dp);
     }
     int minimumDifference(vector<int>& nums) {
         int n = nums.size();
-
         int k = n/2;
 
-        int lsum = accumulate(nums.begin(),nums.end(),0);
+        int sum = accumulate(nums.begin(),nums.end(),0);
 
         vector<vector<int>> dp1(k+1);
         vector<vector<int>> dp2(k+1);
@@ -23,26 +21,28 @@ public:
         solve(0,k,0,0,nums,dp1);
         solve(k,n,0,0,nums,dp2);
 
-        int ans = INT_MAX;
-
-        for(int i = 0;i<dp2.size();i++){
+        for(int i=0;i<=k;i++){
             sort(dp2[i].begin(),dp2[i].end());
         }
 
+        int ans = INT_MAX;
+
         for(int i=0;i<=k;i++){
             for(int j=0;j<dp1[i].size();j++){
-                int curr = dp1[i][j];
-                int target = (lsum - 2*curr)/2;
+                int need = (sum - 2*dp1[i][j])/2;
 
-                auto it = lower_bound(dp2[k-i].begin(),dp2[k-i].end(),target);
+                auto it = lower_bound(dp2[k-i].begin(),dp2[k-i].end(),need);
 
                 if(it!=dp2[k-i].end()){
-                    ans = min(ans,abs(lsum - 2*(curr+*it)));
+                    int val = *it;
+                    ans = min(ans,abs(sum - 2*(dp1[i][j] + val)));
                 }
 
-                if(it!=dp2[k-i].begin()){
+
+                if(it != dp2[k-i].begin()){
                     it--;
-                    ans = min(ans,abs(lsum - 2*(curr+*it)));
+                    int val = *it;
+                    ans = min(ans,abs(sum - 2*(dp1[i][j] + val)));
                 }
             }
         }
