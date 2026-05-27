949d4c98766767123b2428adbbd9df09f895a757
Ayush Bhardwaj <158686408+AyushBhardwajj@users.noreply.github.com>
2026-05-28 03:29:08 +0530

    Time: 44 ms (88.89%) | Memory: 54.7 MB (90.74%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        
-        int n  = s.length();
+        int n = s.length();
 
-        int maxAdjZeroGrp = 0;
-        int prevZeroGrp = -1e9;
+        int prevZeroGroup = -1e9;
+        int currZeroGroup = -1e9;
 
-        int start = 0;
-
-        int oneCnt = 0;
+        char ch = s[0];
+        int ind = 0;
+        int maxAdjZeroGroup = 0;
+        int onecnt= 0;
 
         for(int i=0;i<n;i++){
-            if(s[i] == '1')oneCnt++;
+            if(s[i]=='1')onecnt++;
+            if(s[i]!=ch){
+                if(ch == '0'){
+                    prevZeroGroup = currZeroGroup;
+                    currZeroGroup = i-ind;
+                    maxAdjZeroGroup = max(maxAdjZeroGroup,prevZeroGroup+currZeroGroup);
+                }
+                ind = i;
+                ch = s[i];
+            }
         }
 
-        while(start < n){
-            int end = start;
-
-            while(end<n && s[start] == s[end]){
-                end++;
-            }
-
-            int length = end-start;
-
-            if(s[start] == '0'){
-                maxAdjZeroGrp = max(maxAdjZeroGrp,prevZeroGrp+length);
-
-                prevZeroGrp = length;
-            }
-
-            start = end;
+        if(ch == '0'){
+            prevZeroGroup = currZeroGroup;
+            currZeroGroup = n-ind;
+            maxAdjZeroGroup = max(maxAdjZeroGroup,prevZeroGroup+currZeroGroup);
         }
 
-        int answer = oneCnt + maxAdjZeroGrp;
-
-        return answer;
+        return onecnt+maxAdjZeroGroup;
 
     }
 };
