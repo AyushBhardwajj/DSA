ef3dfc55ca6e908459d6620dd2da22f4becc5b65
Ayush Bhardwaj <158686408+AyushBhardwajj@users.noreply.github.com>
2026-06-03 10:03:21 +0530

    Time: 15 ms (5.67%) | Memory: 12.3 MB (90.03%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
+        string rev = s;
+        reverse(rev.begin(),rev.end());
         int n = s.length();
-        string st = s;
-        reverse(st.begin(),st.end());
-        string temp = s+"#"+st;
-        vector<int> lps(temp.length(),0);
-        int k = temp.length();
 
-        lps[0] = 0;
-        int length = 0;
-
-        for(int i=1;i<k;i++){
-            if(temp[i] == temp[length]){
-                lps[i] = length+1;
-                length++;
-            }
-            else{
-                if(length == 0){
-                    lps[i] = length;
-                }
-                else{
-                    length = lps[length-1];
-                    i--;
-                }
+        for(int i=0;i<s.length();i++){
+            if(!memcmp(s.c_str(),rev.c_str()+i,n-i)){
+                return rev.substr(0,i)+s;
             }
         }
 
-        string culprit = st.substr(0,n-lps[k-1]);
-
-        string ans = culprit+s;
-
-        return ans;
-
-        
+        return rev+s;
     }
 };
