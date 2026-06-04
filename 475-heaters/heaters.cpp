2ded8187b1b36371849d5b1869eb04567673456e
Ayush Bhardwaj <158686408+AyushBhardwajj@users.noreply.github.com>
2026-06-04 10:45:46 +0530

    Time: 7 ms (85.42%) | Memory: 29.1 MB (35.57%) - LeetSync

--- 475-heaters/heaters.cpp
+++ 475-heaters/heaters.cpp
@@ -1,58 +1,31 @@
 class Solution {
 public:
-    bool isRadius(vector<int>& houses,vector<int> &heaters,int mid){
-
-        for(int i=0;i<houses.size();i++){
-            auto it = lower_bound(heaters.begin(),heaters.end(),houses[i]);
-
-            if(it == heaters.end()){
-                it--;
-                int heaterPos = *it;
-
-                if(abs(heaterPos-houses[i])>mid)return false;
-            }
-
-            else if(it == heaters.begin()){
-                int heaterPos = *it;
-
-                if(abs(heaterPos-houses[i])>mid)return false;
-            }
-
-            else {
-                int heaterPos1 = *it;
-                it--;
-                int heaterPos2 = *it;
-
-                if(abs(heaterPos1-houses[i])> mid && abs(heaterPos2-houses[i])>mid)return false;
-            }
-        }
-
-        return true;
-    }
-
     int findRadius(vector<int>& houses, vector<int>& heaters) {
-
-        int start = 0,end = 1e9;
-        sort(houses.begin(),houses.end());
         sort(heaters.begin(),heaters.end());
 
-        int ans = 1e9;
+        int ans = 0;
 
-        while(start<=end){
-            int mid = start + (end-start)/2;
+        for(int i=0;i<houses.size();i++){
+            int pos = houses[i];
 
-            bool flag = isRadius(houses,heaters,mid);
+            int curr = INT_MAX;
 
-            if(flag){
-                ans = mid;
-                end = mid-1;
-            }else{
-                start = mid+1;
+            auto it = lower_bound(heaters.begin(),heaters.end(),pos);
+
+            if(it != heaters.end()){
+                int ind = it-heaters.begin();
+                curr = min(curr,abs(heaters[ind]-houses[i]));
             }
+
+            if(it != heaters.begin()){
+                it--;
+                int ind = it-heaters.begin();
+                curr = min(curr,abs(heaters[ind]-houses[i]));
+            }
+
+            ans = max(ans,curr);
         }
 
         return ans;
     }
-
-
 };
