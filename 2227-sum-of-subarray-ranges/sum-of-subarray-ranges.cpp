9d070a2e51e12f3f4ddab41e91ec1bbcae8f110a
Ayush Bhardwaj <158686408+AyushBhardwajj@users.noreply.github.com>
2026-06-08 11:21:18 +0530

    Time: 5 ms (48.71%) | Memory: 15.6 MB (63.31%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,71 +1,58 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        stack<int> st,st1;
+        stack<int> st1,st2;
         int n = nums.size();
         vector<int> pge(n),pse(n),nge(n),nse(n);
 
         for(int i=0;i<n;i++){
-            while(!st.empty() && nums[i]>=nums[st.top()]){
-                st.pop();
-            }
-
-            while(!st1.empty() && nums[i]<=nums[st1.top()]){
+            while(!st1.empty() && nums[i]>=nums[st1.top()]){
                 st1.pop();
             }
 
-            int ind1 = st.empty()?-1:st.top();
-            int ind2 = st1.empty()?-1:st1.top();
-
-            pge[i] = ind1;
-            pse[i] = ind2;
-
-            st.push(i);
+            pge[i] = st1.empty()?-1:st1.top();
             st1.push(i);
 
-        }
+            while(!st2.empty() && nums[i]<=nums[st2.top()]){
+                st2.pop();
+            }
 
-        while(!st.empty()){
-            st.pop();
+            pse[i] = st2.empty()?-1:st2.top();
+            st2.push(i);
         }
 
         while(!st1.empty()){
             st1.pop();
         }
+        while(!st2.empty()){
+            st2.pop();
+        }
 
         for(int i=n-1;i>=0;i--){
-            while(!st.empty() && nums[i]>nums[st.top()]){
-                st.pop();
-            }
-
-            int ind1 = st.empty()?n:st.top();
-
-            nge[i] = ind1;
-
-            while(!st1.empty() && nums[i]<nums[st1.top()]){
+            while(!st1.empty() && nums[i]>nums[st1.top()]){
                 st1.pop();
             }
 
-            int ind2 = st1.empty()?n:st1.top();
-
-            nse[i] = ind2;
-
-            st.push(i);
+            nge[i] = st1.empty()?n:st1.top();
             st1.push(i);
+
+            while(!st2.empty() && nums[i]<nums[st2.top()]){
+                st2.pop();
+            }
+
+            nse[i] = st2.empty()?n:st2.top();
+            st2.push(i);
         }
 
         long long small = 0,great = 0;
 
         for(int i=0;i<n;i++){
-            small += 1ll*nums[i]*(nse[i]-i)*(i-pse[i]);
             great += 1ll*nums[i]*(nge[i]-i)*(i-pge[i]);
+            small += 1ll*nums[i]*(nse[i]-i)*(i-pse[i]);
         }
 
         long long ans = great-small;
 
         return ans;
-
-
-
     }
 };
