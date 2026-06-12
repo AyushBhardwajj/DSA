6b343811c57a6ee74a5d45d23831e0ea0b28ed1f
Ayush Bhardwaj <158686408+AyushBhardwajj@users.noreply.github.com>
2026-06-12 11:19:22 +0530

    Time: 230 ms (14.28%) | Memory: 260.3 MB (7.14%) - LeetSync

--- 3637-count-number-of-balanced-permutations/count-number-of-balanced-permutations.cpp
+++ 3637-count-number-of-balanced-permutations/count-number-of-balanced-permutations.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    int findpow(long long base,long long pow,long long mod){
-        base = base%mod;
-        long long ans  =1;
-        while(pow){
+    long long binpowmod(long long base,long long pow,long long mod){
+        base = (base)%mod;
+        long long ans = 1;
+
+        while(pow>0){
             if(pow&1){
                 ans = (ans*base)%mod;
             }
@@ -13,69 +14,69 @@ public:
             pow = pow>>1;
         }
 
-        return (int)ans;
+        return ans;
     }
 
-    long long solve(int digit,int eveCnt,int currSum,vector<int>& freq,vector<long long> &invfac,int totalsum,long long totalPerm,int n,vector<vector<vector<long long>>> &dp){
+
+    long long solve(int digit,int cnt,int sum,long long totalperm,vector<long long> &invfac,int n,int totsum,vector<int> &freq,vector<vector<vector<long long>>> &dp){
         if(digit == 10){
-            if((currSum == (totalsum)/2) && eveCnt == (n+1)/2){
-                return totalPerm;
+            if(sum == (totsum)/2 && cnt == (n+1)/2){
+                return totalperm;
             }
+
             return 0;
         }
 
-        if(dp[digit][eveCnt][currSum]!=-1)return dp[digit][eveCnt][currSum];
+        if(dp[digit][cnt][sum]!=-1)return dp[digit][cnt][sum];
 
-
-        long long ways = 0;
         long long mod = 1e9+7;
+        long long ways = 0;
+        
+        for(int i=0;i<=min(freq[digit],(n+1)/2 - cnt);i++){
+            long long inv = (invfac[i]*invfac[freq[digit]-i])%mod;
 
-        for(int i = 0;i<=min(freq[digit],(n+1)/2 - eveCnt);i++){
-            long long div = (invfac[i]*invfac[freq[digit]-i])%mod;
+            long long curr = solve(digit+1,cnt+i,sum+i*digit,totalperm,invfac,n,totsum,freq,dp);
 
-            long long val = solve(digit+1,eveCnt+i,currSum+i*digit,freq,invfac,totalsum,totalPerm,n,dp);
-
-            ways = (ways + (div*val)%mod)%mod;
+            ways = (ways+(curr*inv)%mod)%mod;
         }
 
-        return dp[digit][eveCnt][currSum] = ways;
-    }
+        return dp[digit][cnt][sum] = ways;
 
+    }
     int countBalancedPermutations(string num) {
         int n = num.length();
-        int totalsum = 0;
-        long long mod = 1e9+7;
-
+        int totsum = 0;
         vector<int> freq(10,0);
 
         for(int i=0;i<n;i++){
+            totsum+=(num[i]-'0');
             freq[num[i]-'0']++;
-            totalsum += (int)(num[i]-'0');
         }
 
-        if(totalsum%2)return 0;
+        if(totsum&1)return 0;
+
+        long long mod = 1e9+7;
 
         vector<long long> fact(n+1,1);
-        fact[0] = 1;
-        fact[1] = 1;
+
+        fact[1] =1;
 
         for(int i=2;i<=n;i++){
-            fact[i] = ((fact[i-1]*i)%mod);
+            fact[i] = (fact[i-1]*i)%mod;
         }
 
-        vector<long long> invfact(n+1,1);
+        vector<long long> invfac(n+1,1);
 
-        invfact[n] = findpow(fact[n],mod-2,mod);
+        invfac[n] = binpowmod(fact[n],mod-2,mod);
 
         for(int i=n-1;i>=0;i--){
-            invfact[i] = 1ll*(invfact[i+1]*(i+1))%mod;
+            invfac[i] = (invfac[i+1]*(i+1))%mod;
         }
 
-        long long totalPerm = 1ll*(fact[(n+1)/2]*fact[n/2])%mod;
+        vector<vector<vector<long long>>> dp(10,vector<vector<long long>>((n+3)/2,vector<long long>(361,-1)));
 
-        vector<vector<vector<long long>>> dp(10,vector<vector<long long>>((n+3)/2,vector<long long>(721,-1)));
+        long long totalper = (fact[(n+1)/2]*fact[n/2])%mod;
 
-        return solve(0,0,0,freq,invfact,totalsum,totalPerm,n,dp);
-
+        return (int)solve(0,0,0,totalper,invfac,n,totsum,freq,dp);
     }
 };
