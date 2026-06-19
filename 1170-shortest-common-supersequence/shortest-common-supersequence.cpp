417858eefcdd9e0a4aed6272bf52731d5291b8a0
Ayush Bhardwaj <158686408+AyushBhardwajj@users.noreply.github.com>
2026-06-19 10:37:41 +0530

    Time: 31 ms (10.30%) | Memory: 26.8 MB (42.71%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -6,37 +6,30 @@ public:
 
         vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
 
-        for(int i=1;i<=n1;i++){
-            for(int j=1;j<=n2;j++){
-                if(str1[i-1]==str2[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-                }
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                dp[i+1][j+1] = max(dp[i+1][j],dp[i][j+1]);
+                if(str1[i] == str2[j])dp[i+1][j+1] = max(dp[i+1][j+1],1+dp[i][j]);
             }
         }
 
-        string ans = "";
-
         int last1 = n1,last2 = n2;
 
+        string ans = "";
+
         while(last1>0 && last2>0){
-            if(str1[last1-1]==str2[last2-1]){
+            if(str1[last1-1] == str2[last2-1]){
                 ans.push_back(str1[last1-1]);
                 last1--;
                 last2--;
             }
-
+            else if(dp[last1][last2-1] > dp[last1-1][last2]){
+                ans.push_back(str2[last2-1]);
+                last2--;
+            }
             else{
-                if(dp[last1-1][last2]>dp[last1][last2-1]){
-                    ans.push_back(str1[last1-1]);
-                    last1--;
-                }
-                else{
-                    ans.push_back(str2[last2-1]);
-                    last2--;
-                }
+                ans.push_back(str1[last1-1]);
+                last1--;
             }
         }
 
@@ -53,6 +46,5 @@ public:
         reverse(ans.begin(),ans.end());
 
         return ans;
-
     }
 };
