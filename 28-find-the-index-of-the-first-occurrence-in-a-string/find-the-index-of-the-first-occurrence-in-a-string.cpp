78e5dca2bbfe091db184f57ff57fe355c0cb76b9
Ayush Bhardwaj <158686408+AyushBhardwajj@users.noreply.github.com>
2026-08-31 10:46:51 +0530

    Time: 1 ms (23.59%) | Memory: 9.3 MB (24.89%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,34 +1,49 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        long long base = 1;
-        long long hash1 = 0;
-        long long hash2 = 0;
+        int m = needle.size();
 
-        int n = needle.length();
-        int h = haystack.length();
+        vector<int> lps(m,0);
 
-        long long mod = 1e9+7;
+        int len = 0;
+        int ind = 1;
 
-        for(int i=0;i<n;i++){
-            hash1 = ((hash1*31)%mod + (needle[i]-'a'+1))%mod;
+        while(ind<m){
+            if(needle[ind] == needle[len]){
+                len++;
+                lps[ind] = len;
+                ind++;
+            }
+            else{
+                if(len!=0){
+                    len = lps[len-1];
+                }
+                else{
+                    ind++;
+                }
+            }
         }
 
-        for(int i=0;i<n;i++){
-            hash2 = ((hash2*31)%mod + (haystack[i]-'a'+1))%mod;
-            if(i<n-1)base = (base*31)%mod;
-        }
+        int n = haystack.size();
 
-        if(hash1 == hash2){
-            if(needle == haystack.substr(0,n))return 0;
-        }
+        int i = 0;
+        int j = 0;
+        len = 0;
 
-        for(int i=n;i<h;i++){
-            hash2 = (hash2 - (base*(haystack[i-n]-'a'+1))%mod + mod)%mod;
-            hash2  =  ((hash2*31)%mod + haystack[i]-'a'+1)%mod;
+        while(i<n){
+            if(haystack[i] == needle[j]){
+                i++;
+                j++;
+            }
 
-            if(hash1 == hash2){
-                if(needle == haystack.substr(i-n+1,n))return i-n+1;
+            if(j == m){
+                return i-m;
+            }
+            else if(haystack[i]!=needle[j]){
+                if(j!=0){
+                    j = lps[j-1];
+                }
+                else i++;
             }
         }
 
