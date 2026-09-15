95a9b0d574a316d9cfd2d3bee29daca064c93c27
Ayush Bhardwaj <158686408+AyushBhardwajj@users.noreply.github.com>
2026-09-15 14:49:03 +0530

    Time: 159 ms (51.05%) | Memory: 26.7 MB (25.92%) - LeetSync

---
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    int dp[2000][2000];
+    int solve(int i,int j,string &s,int k){
+        if(i == s.length())return 0;
+        if(j == s.length())return 0;
+
+        int take = 0,nottake = 0;
+
+        if(((j-i+1) >=k) && (dp[i][j]==1)){
+            take = 1+solve(j+1,j+1,s,k);
+        }
+
+        nottake  = solve(i,j+1,s,k)+solve(j+1,j+1,s,k);
+
+        return take+nottake;
+    }
+    int maxPalindromes(string s, int k) {
+        
+        int n = s.length();
+        memset(dp,0,sizeof(dp));
+
+        int i = 0,j=0;
+
+        if(k==1)return n;
+        if(n==1 && k>1)return 0;
+
+        while(j<n){
+            dp[i][j] = 1;
+            i++,j++;
+        }
+        i = 0,j=1;
+
+        while(j<n){
+            if(s[i]==s[j])dp[i][j]=1;
+            i++,j++;
+        }
+        i=0,j=2;
+
+        while(j<n){
+            int nj = j;
+            i = 0;
+
+            while(nj<n){
+                if(s[i]==s[nj] && dp[i+1][nj-1]==1){
+                    dp[i][nj]=1;
+                }
+                i++,nj++;
+            }
+            j++;
+        }
+
+        vector<int> ans(n,0);
+
+        if(n==1)ans[0]=1;
+
+        for(int i=1;i<n;i++){
+            if(dp[0][i]==1 && (i+1)>=k){
+                ans[i] = 1;
+            }
+            for(int j=1;j<=i;j++){
+                if(dp[j][i]==1 && (i-j+1)>=k){
+                    ans[i] = max(ans[i],1+ans[j-1]);
+                }
+                else ans[i] = max(ans[i],ans[j]);
+            }
+        }
+
+        return ans[n-1];
+
+        //return solve(0,0,s,k);
+    }
+};
